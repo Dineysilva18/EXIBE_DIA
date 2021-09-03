7ee6dbfda8b084567eda65a8b49225b42a3b3e9b
Dineysilva18 <46965267+Dineysilva18@users.noreply.github.com>
2021-09-02 22:10:03 -0300

    Update FUNCAO EXIBE_DIA.php

--- FUNCAO EXIBE_DIA.php
+++ FUNCAO EXIBE_DIA.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
+
 <html lang="pt-br">
+   
 <head>
 
    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   
+      <title>FUNÇÃO EXIBE_DIA</title>
+   
+         <!--CSS-->
     
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-    <title>FUNÇÃO EXIBE_DIA</title>
-
-    <!--CSS-->
-    
-    <style type="text/css">
+         <style type="text/css">
          
-       #botao{
+         #botao{
                 display:block;
 
                 float:left;
@@ -21,9 +22,9 @@
             
                 max-width:100%;
 
-                height:30px;
+                height:50px;
 
-                line-height:30px;
+                line-height:50px;
 
                 text-align: center;
 
@@ -33,99 +34,101 @@
 
                 background-color: rgba(0,0,0,0.5);
             
-                padding-left: 1%;
-            
-                padding-right: 1%;
+                padding: 1%;
 
         }
-
-        #botao:hover{
-
-        color: #FFFF00;
             
-        cursor: pointer;
+                #botao:hover{
 
-        }    
+                color: #FFFF00;
+            
+                cursor: pointer;
+
+        }
+            
     </style>
 
 </head>
+   
 <body>
     
-<!-- PHP -->
+   <!-- PHP -->
     
-<?php
-//referência: 01/01/2017 - Domingo => para resto 0 (d-1);
-
-//referência ano bissexto: 2016 => 16;
-
-//Alguns comandos comentados são linhas de código para teste da função.
-
-function processa_dia($day,$month,$year){
-
-if ($year%4==0)
-{
-
-    $extra = 1;
-}else{
-    
-    $extra = 0;
-}
-
-//vetor que referencia a conclusão do mês anterior---------------------------
-
-$mesref = array(0,31,(28+$extra),31,30,31,30,31,31,30,31,30);
-
-//posição 0 = mês 1 (m-1);
-
-//---------------------------------------------------------------------------
-
-$primeiro_resultado=365*($year-2017) + (int)(($year-2017)/4);
-
-//echo "$primeiro_resultado \r\n";
-
-$segundo_resultado = 0;
-
-for($cont=0;$cont<=($month-1);$cont++)
-
-{
-
-$segundo_resultado = $segundo_resultado+$mesref[$cont];
-
-//echo "$segundo_resultado \r\n";
-
-}
-
-return ($day+$primeiro_resultado+$segundo_resultado);
-
-//echo "$resultado_final \r\n";
-
-}
-
-date_default_timezone_set("America/Fortaleza");
-
-//$datalocal = date("d/m/yy - H:i:s");
-
-$dia = date("d");
-
-$mes = date("m");
-
-$ano = date("Y");
-
-$resultado_final = processa_dia($dia,$mes,$ano);
-
-//echo "<b>|$resultado_final|</b> \r\n";
-
-$rest=($resultado_final-1)%7;
-
-$nomedia = array("Domingo","Segunda feira","Terça feira","Quarta feira","Quinta feira","Sexta feira","Sábado");
-?>
-
-<div id="botao">
-<?php 
-   $ano = date("Y");
-   echo "$nomedia[$rest]&nbsp $dia/$mes/$ano";
-   ?>
-</div>
+   <?php
    
-    </body>
+      //referência: 01/01/2017 - Domingo => para resto 0 (d-1);
+
+      //referência ano bissexto: 2016 => 16;
+
+      //Alguns comandos comentados são linhas de código para teste da função.
+
+      function processa_dia($day,$month,$year){
+
+         if ($year%4==0)
+         {
+            
+         $extra = 1;
+            
+         }else{
+    
+         $extra = 0;
+         
+         }
+
+         //vetor que referencia a conclusão do mês anterior---------------------------
+
+         $mesref = array(0,31,(28+$extra),31,30,31,30,31,31,30,31,30);
+
+         //posição 0 = mês 1 (m-1);
+
+         //---------------------------------------------------------------------------
+
+         $primeiro_resultado=365*($year-2017) + (int)(($year-2017)/4);
+
+         //echo "$primeiro_resultado \r\n";
+
+         $segundo_resultado = 0;
+
+         for($cont=0;$cont<=($month-1);$cont++)
+         {
+               
+               $segundo_resultado = $segundo_resultado+$mesref[$cont];
+
+               //echo "$segundo_resultado \r\n";
+         }
+
+         return ($day+$primeiro_resultado+$segundo_resultado);
+
+               //echo "$resultado_final \r\n";
+
+      }
+
+      date_default_timezone_set("America/Fortaleza");
+
+         //$datalocal = date("d/m/yy - H:i:s"); ou $datalocal = date("d/m/Y - H:i:s");
+
+         $dia = date("d");
+
+         $mes = date("m");
+
+         $ano = date("Y");
+
+         $resultado_final = processa_dia($dia,$mes,$ano);
+
+         //echo "$resultado_final \r\n";
+
+         $rest=($resultado_final-1)%7;
+
+         $nomedia = array("Domingo","Segunda feira","Terça feira","Quarta feira","Quinta feira","Sexta feira","Sábado");
+   
+   ?>
+
+   <div id="botao">
+   <?php
+      echo "$nomedia[$rest]&nbsp $dia/$mes/$ano";
+   ?>
+   </div>
+   
+</body>
+   
 </html>
